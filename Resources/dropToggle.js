da5c698f775c721d25d7666603499dcc3899232d
John <107654605+JohnTorgerson@users.noreply.github.com>
2022-09-24 15:07:28 -0500

    trying some dropdown code

--- Resources/dropToggle.js
+++ Resources/dropToggle.js
@@ -1,1 +1,36 @@
+// Id this script
+console.log("Dropdown Toggle switcher")
 
+// Getch JSON Data and console log it
+const biodiversity = "data/samples.json"
+
+d3.json(biodiversity).then(function(data) {
+    console.log(data);
+});
+
+
+
+
+
+// // Select the counter h3 element
+// let counter = d3.select(".counter");
+
+// // Select the buttons and use D3 `.on` to attach a click handler
+// d3.selectAll("button").on("click", function() {
+
+//   // Create a variable for the button selected
+//   let button = d3.select(this);
+
+//   // Create a varaible to hold the increment or decrement
+//   let vote = parseInt(button.attr('value'))
+
+//   // Create a variable to hold the current counter value
+//   let currentCount = parseInt(counter.text())
+
+//   // Update the counter value
+//   currentCount += vote;
+
+//   // Set the counter h3 text to the new count
+//   counter.text(currentCount);
+
+// });
